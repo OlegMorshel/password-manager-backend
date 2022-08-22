61805732a5264ab63a9f572406df8d39ef97ca2a
Oleg <morshel@addamant.ru>
2022-08-22 10:54:57 +0300

    feat: authGuard to passwordList

--- src/password-list/password-list.controller.ts
+++ src/password-list/password-list.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
+import { JswAuthGuard } from 'src/auth/jwt-ath.guard';
 import { CreatePasswordListDto } from './dto/create-password-list.dto';
 import { PasswordListService } from './password-list.service';
 
 @Controller('password-list')
 export class PasswordListController {
   constructor(private passwordListService: PasswordListService) {}
+  @UseGuards(JswAuthGuard)
   @Post()
   create(@Req() res: Request, @Body() passwordList: CreatePasswordListDto) {
     return this.passwordListService.createList(res, passwordList);
   }
-
   @Get()
+  @UseGuards(JswAuthGuard)
   getAll() {
     return this.passwordListService.getAll();
   }
